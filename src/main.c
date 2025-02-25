64555ff82a3adac06aa2e74a2f4b7a8b06081f2b
Yeva Biloshytska <ybiloshy@c2r6s5.42wolfsburg.de>
2025-02-25 15:57:52 +0100

    without astricks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ybiloshy <ybiloshy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 15:56:42 by sikunne           #+#    #+#             */
-/*   Updated: 2025/02/25 15:52:46 by ybiloshy         ###   ########.fr       */
+/*   Updated: 2025/02/25 15:56:43 by ybiloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv, char **envp)
 {
 	char **new_env;
 
-	(void)argv[0][0];
+	(void)argv;
 	(void)argc;
 	new_env = ft_copy_env(envp);
 	return (ft_loop(&new_env));
