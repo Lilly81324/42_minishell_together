12c0e7fb5ab22abfd3afdb011f0290f12c6a5cea
Yeva Biloshytska <ybiloshy@c2r6s5.42wolfsburg.de>
2025-02-25 15:53:48 +0100

    ch argv

--- src/main.c
+++ src/main.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ybiloshy <ybiloshy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 15:56:42 by sikunne           #+#    #+#             */
-/*   Updated: 2025/02/24 17:59:00 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/02/25 15:52:46 by ybiloshy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
 	char **new_env;
 
-	if (argc == 2)
-		printf("%s", argv[1]);
-	new_env = ft_copy_env(envp);	
+	(void)argv[0][0];
+	(void)argc;
+	new_env = ft_copy_env(envp);
 	return (ft_loop(&new_env));
 }
 
