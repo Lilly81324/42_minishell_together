ea62161396ead62d34196bb0be8c17699375bf40
Lilly Kunne <sikunne@c2r10s8.42wolfsburg.de>
2025-02-26 15:31:52 +0100

    Applied last commits policy to one more file I originally overlooked

--- src/ft_token_skip_chunk.c
+++ src/ft_token_skip_chunk.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:32:14 by sikunne           #+#    #+#             */
-/*   Updated: 2025/02/17 16:35:39 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/02/26 15:30:54 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 // -> would set *i to 2
 void	ft_token_skip_chunk(char *arg[], int *i)
 {
-	while (arg[*i] != NULL && arg[*i][0] != '|' && arg[*i][0] != ';')
+	while (ft_is_delimiter(arg[*i]) != 1)
 		(*i)++;
 }
