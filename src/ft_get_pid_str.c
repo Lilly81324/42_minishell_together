a39fa4bc13e9ab0b638f0b900214098f278fe2a4
Lilly Kunne <sikunne@c2r14s4.42wolfsburg.de>
2025-03-19 17:50:14 +0100

    Norm fix

--- src/ft_get_pid_str.c
+++ src/ft_get_pid_str.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:16:01 by sikunne           #+#    #+#             */
-/*   Updated: 2025/03/19 17:46:05 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/03/19 17:49:46 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ char	*ft_get_pid_str(void)
 	fd = open("/proc/self/stat", O_RDONLY);
 	if (fd < 0)
 		return (NULL);
-	readd = read(fd, buf, sizeof(buf) -1);
+	readd = read(fd, buf, sizeof(buf) - 1);
 	buf[99] = '\0';
 	close(fd);
 	if (readd <= 0)
