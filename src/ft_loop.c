73b2757141b32cbea5e40703c690a07838156ff6
Lilly Kunne <sikunne@c2r5s8.42wolfsburg.de>
2025-03-27 18:16:10 +0100

    Fixed function name typo

--- src/ft_loop.c
+++ src/ft_loop.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:46:42 by sikunne           #+#    #+#             */
-/*   Updated: 2025/03/27 18:09:01 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/03/27 18:15:46 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	ft_loop(char ***envp)
 		shl.heredoc_pos = 0;
 		shl.start = NULL;
 		prompt = ft_make_prompt(envp);
-		input = st_my_readline(&prompt);
+		input = ft_my_readline(&prompt);
 		add_history(input);
 		status = ft_handle_input(&input, &shl);
 		ft_null(&input);
