3e5cfa28a110100ac42c21e6ce1cad58e78dc2ea
Lilly Kunne <sikunne@c1r12s5.42wolfsburg.de>
2025-04-03 15:44:20 +0200

    Added notation for adding semicolon handling

--- src/ft_is_delimiter.c
+++ src/ft_is_delimiter.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:48:00 by sikunne           #+#    #+#             */
-/*   Updated: 2025/04/03 15:37:01 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/04/03 15:42:54 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 // returns 1 if string is exactly
 // NULL, "|" or ";"
 // otherwise returns 0
+// For SEMICOLON handling change semicolon check return to 1
 int	ft_is_delimiter(char *str)
 {
 	if (str == NULL)
--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:42:19 by sikunne           #+#    #+#             */
-/*   Updated: 2025/04/03 15:36:50 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/04/03 15:43:43 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,7 @@
 
 // Used in ft_tokenization to know what to skip over
 # define SPACES " \n\t\v\f\r"
+// For SEMICOLON handling add semicolon to SPECIALS, more in ft_isdelimiter
 # define SPECIALS "><|"
 // Used to define how many times argument substitution is called on same string
 # define MAX_SUBSTITUTIONS 10
