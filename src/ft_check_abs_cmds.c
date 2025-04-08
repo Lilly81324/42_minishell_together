b824772fb202b81e853eac6d77a3cda859aef818
Lilly Kunne <sikunne@c2r8s3.42wolfsburg.de>
2025-04-08 21:08:21 +0200

    Fixed paht/file displaying wrong error

--- src/ft_check_abs_cmds.c
+++ src/ft_check_abs_cmds.c
@@ -6,7 +6,7 @@
 /*   By: sikunne <sikunne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 15:18:14 by sikunne           #+#    #+#             */
-/*   Updated: 2025/04/08 20:52:32 by sikunne          ###   ########.fr       */
+/*   Updated: 2025/04/08 21:02:32 by sikunne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int	ft_check_abs_cmds(char **token, int pos)
 {
 	if (token[pos] == NULL)
 		return (0);
-	if (token[pos][0] == '/')
+	if (ft_find_c('/', token[pos]) != -1)
 		return (1);
 	if (token[pos][0] == '.' && token[pos][1] == '\0')
-		return (1);
-	if (ft_strncmp(token[pos], "./", 2) == 0)
 		return (1);
 	if (ft_strncmp(token[pos], "..", 2) == 0)
 		return (1);
